308cd0e5f4771e4bd14a27945c5d9c1bf81c1384
Jun Kurihara <kurihara@ieee.org>
2022-02-15 20:27:15 +0900

    implement RS code-based GD

--- src/dict.rs
+++ src/dict.rs
@@ -106,8 +106,80 @@ where
       .map_err(|_| anyhow!("Invalid dictionary"))?;
 
     // 3. return the id.
+    // println!(
+    // "{:?} {:?} {:?} {:?}\n{:?}\n{:?}",
+    // kr, k, vr, v, self.dict_base_to_id, self.dict_id_to_base
+    // );
     ensure!(kr == k && vr == v, "Failed to remove...Broken dictionary");
 
     Ok(v)
+  }
+
+  pub fn check_inner_integrity(&self) -> Result<()> {
+    // check consistency between id_to_base and base_to_id
+    ensure!(
+      self.dict_base_to_id.len() == self.dict_id_to_base.len(),
+      "Different size of dictionary"
+    );
+
+    let res: Vec<Result<()>> = self
+      .dict_base_to_id
+      .iter()
+      .map(|(base, id)| {
+        if let Some(base_r) = self.dict_id_to_base.get(id) {
+          if *base_r == *base {
+            Ok(())
+          } else {
+            Err(anyhow!("a base is inconsistent for id {}", id))
+          }
+        } else {
+          Err(anyhow!("a base is missing in dict_id_to_base"))
+        }
+      })
+      .collect();
+    ensure!(res.iter().all(|x| (*x).is_ok()), "Inconsistent dictionary");
+
+    Ok(())
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  const WORD_STR: &str = "寿限無(じゅげむ)寿限無(じゅげむ)五劫(ごこう)のすりきれ海砂利(かいじゃり)水魚(すいぎょ)の水行末(すいぎょうまつ) ";
+  const MSG_BYTELEN: usize = 3;
+  const CODE_BYTELEN: usize = 4;
+  const DICT_SIZE: usize = 10;
+
+  #[test]
+  fn dict_u8_sync_works() {
+    let word_bytes = WORD_STR.to_string().repeat(128).into_bytes();
+    let mut basis_dict_1 = BasisDict::<U8VRep>::new(DICT_SIZE);
+    let mut basis_dict_2 = BasisDict::<U8VRep>::new(DICT_SIZE);
+
+    let mut byte_ptr: usize = 0;
+    while byte_ptr < word_bytes.len() {
+      if byte_ptr + MSG_BYTELEN > word_bytes.len() {
+        break;
+      }
+
+      let buf = &word_bytes[byte_ptr..byte_ptr + MSG_BYTELEN];
+      if let Some(bit_id) = basis_dict_1.get_id(&buf.to_vec()) {
+        let base_r = basis_dict_2.get_base(&bit_id).unwrap();
+        assert_eq!(base_r, buf.to_vec());
+      } else {
+        let _new_id = basis_dict_1.put_base(&buf.to_vec()).unwrap();
+        // simulate existing usecase
+        let mut bv_buf = BVRep::from_slice(&buf.to_vec()).clone();
+        bv_buf.force_align();
+        let u8buf = bv_buf.to_bitvec().as_raw_slice().to_owned();
+        let _new_id_r = basis_dict_2.put_base(&u8buf).unwrap();
+        assert_eq!(_new_id, _new_id_r);
+      }
+      assert!(basis_dict_1.check_inner_integrity().is_ok());
+      assert!(basis_dict_2.check_inner_integrity().is_ok());
+
+      byte_ptr += CODE_BYTELEN;
+    }
   }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -111,7 +111,6 @@ where
   }
 
   fn dedup(&mut self, buf: &U8SRep) -> Result<Deduped> {
-    let code_bytelen = self.code.code_byte_len();
     let last_chunk_pad_bytelen = self.chunk_bytelen - buf.len() % self.chunk_bytelen;
     let mut padded = vec![0u8; last_chunk_pad_bytelen];
 
@@ -152,8 +151,55 @@ where
     })
   }
   fn dup(&mut self, deduped: &Deduped) -> Result<U8VRep> {
-    //TODO:
-    Ok(vec![0u8; 10])
+    let u8size = u8::BITS as usize;
+    let code_bitlen = self.code.code_byte_len() * u8size;
+    let info_bitlen = self.code.info_byte_len() * u8size;
+    let dev_bitlen = code_bitlen - info_bitlen;
+    let id_bitlen = self.basis_dict.id_bitlen();
+    let mut res = U8VRep::new();
+
+    let mut bitptr = 0usize;
+    while bitptr < deduped.data.len() {
+      let sep = match deduped.data[bitptr] {
+        false => Separator::AsIs,
+        true => Separator::Deduped,
+      };
+      bitptr += 1;
+
+      let (base, step) = match sep {
+        Separator::AsIs => {
+          let mut bv = deduped.data[bitptr..bitptr + info_bitlen]
+            .to_bitvec()
+            .clone();
+          bv.force_align();
+          let part = bv.as_raw_slice().to_owned();
+          let _new_id = self.basis_dict.put_base(&part)?;
+          ((&part).to_owned(), info_bitlen)
+        }
+        Separator::Deduped => {
+          let id = deduped.data[bitptr..bitptr + id_bitlen].to_owned();
+          (self.basis_dict.get_base(&id)?, id_bitlen)
+        }
+      };
+      bitptr += step;
+
+      let mut bv = deduped.data[bitptr..bitptr + dev_bitlen]
+        .to_bitvec()
+        .clone();
+      bv.force_align();
+      let dev = bv.as_raw_slice().to_owned();
+      bitptr += dev_bitlen;
+
+      let encoded = self.code.encode(&base, &dev)?;
+      let target = if bitptr == deduped.data.len() {
+        &encoded.errored[deduped.last_chunk_pad_bytelen..]
+      } else {
+        &encoded.errored[..]
+      };
+      res.extend_from_slice(target);
+    }
+
+    Ok(res)
   }
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -248,7 +294,7 @@ where
       ensure!(target_bitslice.len() % 8 == 0, "Invalid target in dup");
       res.extend_from_bitslice(target_bitslice);
     }
-    assert_eq!(bitptr, deduped.data.len());
+    ensure!(bitptr == deduped.data.len(), "Invalid deduped data length");
 
     Ok(res.as_raw_slice().to_owned())
   }
@@ -260,46 +306,70 @@ where
 mod tests {
   use super::*;
 
-  const WORD_STR: &str = "寿限無(じゅげむ)寿限無(じゅげむ)五劫(ごこう)のすりきれ海砂利(かいじゃり)水魚(すいぎょ)の水行末(すいぎょうまつ) ";
+  // const WORD_STR: &str = "寿限無(じゅげむ)寿限無(じゅげむ)五劫(ごこう)のすりきれ海砂利(かいじゃり)水魚(すいぎょ)の水行末(すいぎょうまつ) ";
+  // const WORD_STR: &str = "寿限無(じゅげむ)寿限無(じゅげむ)五劫(ごこう)のすりきれ海砂利(かいじゃり)水魚(すいぎょ)の水行末(すいぎょうまつ) 雲来末(うんらいまつ) 風来末(ふうらいまつ)食(く)う寝(ね)るところに住(す)むところやぶらこうじのぶらこうじパイポパイポパイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナの長久命(ちょうきゅうめい)の長助(ちょうすけ)";
+  const WORD_STR: &str =
+    "寿限無(じゅげむ)寿限無(じゅげむ)五劫(ごこう)のすりきれ海砂利(かいじゃり)padpadpadpadpadpadpadpad"; // Byte alignment is quire needed...
 
   #[test]
   fn hamming_works() {
     for hamming_deg in 4..11 {
       let hamming_dict_size = 511;
-      let mut gd_dedup = GD::Hamming(hamming_deg).setup(hamming_dict_size).unwrap();
-      gd_dedup.unit_check();
 
-      // let words: U8VRep = (0..255).into_iter().collect();
-      let words = WORD_STR.to_string().repeat(128).into_bytes();
-      println!("org size: {} bits", words.len() * 8);
-      let x = gd_dedup.dedup(&words).unwrap();
-      println!("deduped size {} bits", x.data.len());
+      let mut gd_dedup = GD::Hamming(hamming_deg).setup(hamming_dict_size).unwrap();
       let mut gd_dup = GD::Hamming(hamming_deg).setup(hamming_dict_size).unwrap();
+      // gd_dedup.unit_check();
+
+      // println!("Hamimng code deg = {}", hamming_deg);
+      let words = WORD_STR.to_string().repeat(128).into_bytes();
+      // println!("> org size: {} bits", words.len() * 8);
+      let x = gd_dedup.dedup(&words).unwrap();
+      // println!("> deduped size {} bits", x.data.len());
       let y = gd_dup.dup(&x).unwrap();
-      // println!("{:?}", y);
-      println!("duped size {} bits", y.len() * 8);
+      // println!("> duped size {} bits", y.len() * 8);
       assert_eq!(y, words);
+      println!(
+        "Hamimng code deg = {} > Deduped rate: {:.2} %",
+        hamming_deg,
+        100.0 * (x.data.len() as f32) / ((y.len() * 8) as f32)
+      );
+      // println!()
     }
   }
 
+  const RS_MAX_DICT_BITS: usize = 16;
+  const RS_DICT_PARAM: usize = 4;
+
   #[test]
   fn rs_works() {
-    let code_len = 4;
-    let msg_len = 3;
-    let dict_size = 1024;
+    println!("{}", WORD_STR.bytes().len());
+    for code_len in vec![16, 32, 64, 128].into_iter() {
+      for msg_len in 2isize.max(code_len as isize - 8) as usize..code_len {
+        let dict_size = (1 << ((code_len - msg_len) * RS_DICT_PARAM).min(RS_MAX_DICT_BITS)) - 1;
 
-    let mut gd_dedup = GD::ReedSolomon(code_len, msg_len).setup(dict_size).unwrap();
-    let mut gd_dup = GD::ReedSolomon(code_len, msg_len).setup(dict_size).unwrap();
+        let mut gd_dedup = GD::ReedSolomon(code_len, msg_len).setup(dict_size).unwrap();
+        let mut gd_dup = GD::ReedSolomon(code_len, msg_len).setup(dict_size).unwrap();
+        // gd_dedup.unit_check();
 
-    let words = WORD_STR.to_string().repeat(128).into_bytes();
+        let words = WORD_STR.to_string().repeat(128).into_bytes();
 
-    gd_dedup.unit_check();
-    println!("> org size: {} bits", words.len() * 8);
-    let x = gd_dedup.dedup(&words).unwrap();
-    println!("> deduped size {} bits", x.data.len());
-    let y = gd_dup.dup(&x).unwrap();
-    // println!("{:?}", y);
-    println!("> duped size {} bits", y.len() * 8);
-    // println!("{:?}", gd);
+        // println!("RS code ({}, {}) over GF(256)", code_len, msg_len);
+        // println!("> org size: {} bits", words.len() * 8);
+        let x = gd_dedup.dedup(&words).unwrap();
+        // println!("> deduped size {} bits", x.data.len());
+        let y = gd_dup.dup(&x).unwrap();
+        // println!("> duped size {} bits", y.len() * 8);
+        assert_eq!(y, words);
+        // println!("{:?}", gd);
+        println!(
+          "RS code ({}, {}) over GF(256) of dict size {} > Deduped rate: {:.2} %",
+          code_len,
+          msg_len,
+          dict_size,
+          100.0 * (x.data.len() as f32) / ((y.len() * 8) as f32)
+        );
+        // println!()
+      }
+    }
   }
 }
